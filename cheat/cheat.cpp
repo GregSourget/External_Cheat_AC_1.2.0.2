5e899e91fcf8fd523479f0390b54d55951b3c49b
GregSourget <sourgetgregoire@gmail.com>
2024-11-17 21:56:42 +0100

    fichiers de base

---
+++ cheat/cheat.cpp
@@ -0,0 +1,98 @@
+#include <Windows.h>
+#include <vector>
+#include <iostream>
+#include <CommCtrl.h>
+#include "../cheat/offset.h"
+#include "../cheat/memory.h"
+#include "../cheat/cheat.h"
+
+
+int cheat::updatedHealth = 100; //initalisation
+int cheat::updatedNade = 0;
+int cheat::updatedAmmo = 20;
+int initialHealth = 0;
+int initialNade = 0;
+int initialAmmo = 0;
+bool cheat::isNoRecoilOn = false; // check 
+bool cheat::isInfNadeOn = false;
+bool cheat::isInfAmmoOn = false;
+bool cheat::isGodModeOn = false;  
+bool cheat::isGetInfoOn = false;
+std::uintptr_t cheat::headPtr = 0;
+std::uintptr_t cheat::entityL = 0;
+std::uintptr_t cheat::entity = 0;
+float cheat::headValue = 0.0f;
+
+void cheat::godmodeon() noexcept
+{
+    if (isGodModeOn)
+        return;
+
+    auto& memory = getMemory();
+    const auto moduleBase = memory.GetModuleAddress("ac_client.exe");
+    const auto localPlayerPtr = memory.Read<std::uintptr_t>(moduleBase + localPlayer);
+    const auto healthAddress = localPlayerPtr + m_iHealth;
+
+    initialHealth = memory.Read<int>(healthAddress);
+
+    isGodModeOn = true;
+
+    int updatedHealth = 9999;
+    memory.Write<int>(healthAddress, updatedHealth);
+}
+
+
+void cheat::godmodeoff() noexcept
+{
+    if (!isGodModeOn)
+        return;
+
+    auto& memory = getMemory();
+    const auto moduleBase = memory.GetModuleAddress("ac_client.exe");
+    const auto localPlayerPtr = memory.Read<std::uintptr_t>(moduleBase + localPlayer);
+    const auto healthAddress = localPlayerPtr + m_iHealth;
+
+    isGodModeOn = false;
+
+    memory.Write<int>(healthAddress, initialHealth);
+}
+
+void cheat::infnadeon() noexcept 
+{
+    
+}
+
+
+void cheat::infnadeoff() noexcept
+{
+    
+}
+
+void cheat::infammoon() noexcept
+{
+    
+}
+
+void cheat::infammooff() noexcept
+{
+    
+}
+
+
+
+void cheat::norecoilon() noexcept 
+{
+    
+}
+
+void cheat::norecoiloff() noexcept 
+{
+    
+}
+
+
+void cheat::getinfoon() noexcept {
+    
+}
+
+
