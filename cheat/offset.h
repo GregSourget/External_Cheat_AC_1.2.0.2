150119dd0ebc6944edf3a04e7635d3fd1672e5ce
Dokka01 <clement.boods@gmail.com>
2024-11-22 09:19:44 +0100

    InfAmmo Pistol

--- cheat/offset.h
+++ cheat/offset.h
@@ -23,6 +23,7 @@ constexpr auto dwViewMatrix = 0x501AE8;
 // ammo nade function recoil
 constexpr auto m_Nades = 0x158;
 constexpr auto m_Ammo = 0x150;
+constexpr auto m_AmmoPistol = 0x13C;
 constexpr auto m_recoil1 = 0x63786;
 
 
