5366a5ac9087901f208935e73aca054898b4f144
GregSourget <sourgetgregoire@mail.com>
2024-12-16 11:17:01 +0100

    up

--- cheat/Struct.h
+++ cheat/Struct.h
@@ -1,27 +1,27 @@
-//#pragma once
-//
-//typedef struct
-//{
-//	float x, y;
-//} vec2d_f;
-//
-//typedef struct
-//{
-//	float x, y, z;
-//} vec3d_f;
-//
-//
-//class Entity {
-//
-//public:
-//
-//	char name[20];
-//	int base;
-//	int health;
-//	vec3d_f position_head;
-//	vec3d_f position_feet;
-//	int team;
-//
-//	void Print();
-//};
-//
+#pragma once
+
+typedef struct
+{
+	float x, y;
+} vec2d_f;
+
+typedef struct
+{
+	float x, y, z;
+} vec3d_f;
+
+
+class Entity {
+
+public:
+
+	char name[20];
+	int base;
+	int health;
+	vec3d_f position_head;
+	vec3d_f position_feet;
+	int team;
+
+	void Print();
+};
+
