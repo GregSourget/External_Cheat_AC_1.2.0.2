d69a1a296638246c53a066124fef26eb9120f4bf
GregSourget <sourgetgregoire@mail.com>
2025-01-12 18:50:48 +0100

    aimbot keyboard shortcut

---
+++ cheat/keyboard_shortcut.cpp
@@ -0,0 +1,33 @@
+#include "cheat.h"
+#include <Windows.h>
+#include <iostream>
+#include <thread>
+
+void listenForHotkeys() {
+    while (true) {
+        if (GetAsyncKeyState('A') & 0x8000) {
+            if (cheat::isAimBotOn) {
+                cheat::aimboton();
+            }
+            else {
+                cheat::aimbotoff();
+            }
+            while (GetAsyncKeyState('A') & 0x8000) {
+                std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            }
+        }
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    }
+}
+
+int main() {
+    std::thread hotkeyThread(listenForHotkeys);
+    hotkeyThread.detach();
+
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+
+    return 0;
+}
